3bb006324860c668c538c611f41652acfe084b91
Relihazard <relihazard@gmail.com>
2015-09-02 15:49:09 +0200

    Recap form

---
+++ projet Coloretto Da Rocha Grossmann/projet Coloretto Da Rocha Grossmann/Recap.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace projet_Coloretto_Da_Rocha_Grossmann
+{
+    public partial class Recap : Form
+    {
+        Jeu coloretto = new Jeu();
+        List<string> paquetCartes = new List<string>();
+        List<int> scoreJoueurs = new List<int>();
+        public Recap(Jeu coloretto, List<string>paquetCartes, List<int>scoreJoueurs)
+        {
+            InitializeComponent();
+            this.coloretto = coloretto;
+            this.paquetCartes = paquetCartes;
+            this.scoreJoueurs = scoreJoueurs;
+        }
+    }
+}
